125233ade4260a032df18f340f86d3cce68ec31f
mosnyder <m.sierrasnyder@gmail.com>
2018-02-12 22:01:00 -0800

    some vids

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -4,6 +4,8 @@ import { range } from 'lodash'
 import { Row, Col, Container } from 'glamorous-grid'
 
 import Collage from '../../assets/cornell.jpg'
+import Engineer from '../../assets/engineers-clip.mov'
+import SweetBites from '../../assets/sb-clip.mov'
 
 const images = range(0, 18).map(i => Collage)
 
@@ -12,9 +14,27 @@ const Image = glamorous.img({
 	maxWidth: '100%'
 })
 
+const Video = glamorous.video({
+	maxHeight: '500px',
+	maxWidth: '100%',
+	autoplay: true
+})
+
 export default () => (
   <Div>
   	<Row>
+  		<Col span={2/3}>
+  			 <Video loop autoPlay src={SweetBites} />
+  		</Col>
+  		<Col span={1/3}>
+  			<Image src={Collage} />
+  		</Col>
+  		<Col span={1/3}>
+  			<Image src={Collage} />
+  		</Col>
+  		<Col span={2/3}>
+  			<Video loop autoPlay src={Engineer} />
+  		</Col>
 	  	{images.map(img => (
 	  		<Col span={1/3}>
 	  			<Image src={img} alt="halp" />
